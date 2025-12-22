f2e5f3dee39c2cd29de517b52cc3d86b1e4a1a87
“Musician” <hhajriwnko@gmail.com>
2025-12-22 20:56:11 +0530

    Add Telegraf integration and update dependencies; remove unused constants file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-import { app } from "./app.js";
+import { Telegraf } from 'telegraf';
+import { channelPost, message } from 'telegraf/filters';
 import dotenv from 'dotenv';
-const port = 3000;
 
 const result = dotenv.config({
-  path: "./.env",
+  path: "./../.env",
 });
 //env is loaded or not.
 if(result.error){
@@ -12,11 +12,54 @@ if(result.error){
   console.log("Environment varialbes loaded -> Successfully");
 }
 
-app.on('error', (error)=>{
-  console.log("Error : ",error);
-  throw error;
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.command('quit', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.leaveChat(ctx.message.chat.id)
+
+  // Using context shortcut
+  await ctx.leaveChat()
 })
 
-app.listen(port || 8080, () => {
-  console.log(`App listening on port http://localhost:${process.env.PORT}`)
+bot.on(message('text'), async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+
+  // Using context shortcut
+  await ctx.reply(`Hello ${ctx.state}`)
+})
+
+// bot.on('channel_post',(ctx) => {
+//   console.log(ctx.update.channel_post.text)
+//   ctx.reply("I saw a channel post");
+// });
+
+bot.on('channel_post',(ctx) => {
+  console.log(ctx.update)
+  ctx.reply("I saw a channel post");
 });
+
+
+bot.on('callback_query', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+
+  // Using context shortcut
+  await ctx.answerCbQuery()
+})
+
+bot.on('inline_query', async (ctx) => {
+  const result = []
+  // Explicit usage
+  await ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+
+  // Using context shortcut
+  await ctx.answerInlineQuery(result)
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
